f9c9afd9ab0a14093adaed7b1f943d583075ca57
Omkar Gavade <omkarvgawade@gmail.com>
2021-09-01 15:57:36 +0530

    portfolio sidebar is created

---
+++ portfolio/src/Components/Navigation.jsx
@@ -0,0 +1,105 @@
+import React from 'react'
+import styled from 'styled-components'
+import { NavLink } from 'react-router-dom'
+import avatar from '../Images/avatar.png'
+function Navigation() {
+    return (
+        <NavgiationStyled>
+            <div className="avatar">
+                <img src={avatar} alt="" />
+            </div>
+            <ul className="nav-items">
+                <li className="nav-item">
+                    <NavLink to="/home" activeClassName="active-class">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about" activeClassName="active-class">About</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/resume" activeClassName="active-class">Resume</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/Portfolios" activeClassName="active-class">Portfolios</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/blogs" activeClassName="active-class">Blogs</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/contact" activeClassName="active-class">Contact</NavLink>
+                </li>
+
+            </ul>
+            <footer className="footer">
+                <p>@2021 My PortFolio Website</p>
+            </footer>
+        </NavgiationStyled>
+    )
+}
+
+
+
+
+const NavgiationStyled = styled.nav`
+display: flex;
+justify-content: space-between;
+flex-direction: column;
+align-items: center;
+height: 100%;
+width: 100%;
+border-right: 1px solid var(--border-color) ;
+.avatar{
+    width: 70%;
+    border-bottom:1px solid var(--border-color);
+    text-align:center;
+       padding: 1rem 0;
+    img{
+        width: 100%;
+        border-radius:50%;
+        border: 8px solid  var(--border-color);
+     
+    }
+}
+.nav-items{
+    width: 100%;
+    text-align: center;
+    li{
+        display: block;
+        a{
+            display: block;
+            padding: .2rem 0;
+            position: relative;
+           
+            &:hover{
+                cursor: pointer;
+               
+            }
+            &::before{
+                content: "";
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                width:0;
+                height: 50%;
+                background-color:red;
+                 transition: All 0.4s ease-in-out cubic-bezier(1,-0.2,.25,.95);
+            }
+        }
+        a:hover::before{
+            width: 100%;
+            height: 100%;
+        }
+    }
+}
+.footer{
+    border-top: 1px solid var(--border-color);
+    width: 100%;
+    p{
+        padding: 2rem 0;
+        font-size: 1.1rem;
+        display: block;
+        text-align: center;
+    }
+}
+
+`
+export default Navigation
---
+++ portfolio/src/Styles/GlobalStyles.jsx
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+
+:root{
+    --primary-color:#007bff;
+    --secondary-color:#6c75d;
+    --background-dark-color:#10121A;
+    --border-color:#2e344e;
+    --background-light-color:#F1F1F1;
+    --white-color:#FFF;
+    --font-light-color:#a4acc4;
+    --font-dark-color:#313131;
+    --font-dark-color-2:#151515;
+    --sidebar-dark-color:#191D2B;
+}
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing:border-box;
+    list-style: none;
+    text-decoration: none;
+    font-size: 1.2rem;
+}
+body{
+    background-color:var(--background-dark-color);
+    color:var(--font-light-color)
+}
+a{
+    font-family:inherit;
+    color:inherit;
+    font-size: inherit;
+}
+
+
+`
